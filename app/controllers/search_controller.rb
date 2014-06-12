6b52a62d374d515bbb5f907a9a36aaa9b1daf6a3
Chris Fung <aergonaut@gmail.com>
2014-06-12 13:51:41 -0700

    actually be smart about this

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,9 +6,13 @@ class SearchController < ApplicationController
   
   def pulls
     @pulls = pulls_for_branch(params[:branch])
+    @issues = issues_for_pulls(*@pulls.map { |pull| pull["number"] })
+    @issues = @issues.items.inject({}) do |hash, issue|
+      hash["##{issue['number']}"] = issue
+      hash
+    end
     @pulls = @pulls.map do |pull|
-      issue = issue_for_pull(pull["number"])
-      pull["labels"] = issue["labels"]
+      pull["labels"] = @issues["##{pull["number"]}"]["labels"]
       pull
     end
     respond_to do |format|
@@ -22,8 +26,8 @@ class SearchController < ApplicationController
     client.pull_requests(ENV["HIKU_GITHUB_REPOSITORY"], base: branch)
   end
   
-  def issue_for_pull(pull_number)
-    client.issue(ENV["HIKU_GITHUB_REPOSITORY"], pull_number)
+  def issues_for_pulls(*pulls)
+    client.search_issues("#{pulls.join(' ')} repo:#{ENV['HIKU_GITHUB_REPOSITORY']}")
   end
   
   def client
